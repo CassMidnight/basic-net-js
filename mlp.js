85daa990501849530c25ebc63e6025201dbb4276
Anneke <cox.anneke@gmail.com>
2021-04-11 23:14:22 +1000

    Added bias and minor refactoring

--- mlp.js
+++ mlp.js
@@ -1,3 +1,7 @@
+// Sources:
+// https://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/
+// Artificial Intelligence: A guide to Intelligent Systems - Michael Negnevitsky
+
 let nodeTemplate = {
   // Feed Forward
   netValue: 0.5,
@@ -50,11 +54,132 @@ function randomiseNodes(model, debugMode = false){
       //console.log(model[layerIndex][nodeIndex].weights);
 
       // You may want to comment this back in later
-      //model[layerIndex][nodeIndex].bias = Math.random();
+      model[layerIndex][nodeIndex].bias = Math.random();
     }
   }
   //logObject("randomised model", model);
 }
+
+
+
+function forwardPass(model, inputData){
+  inputData.forEach((dataPoint, index) => {
+    if (index < model[0].length){
+      model[0][index].value = dataPoint;
+    }
+  })
+
+  for (let i = 0; i < model.length - 1; i++){
+    calculateLayers(model[i], model[i+1])
+  }
+
+  //Return an array of the values of the nodes
+  return model[model.length -1].map(node => {
+    return node.value;
+  })
+}
+
+function calculateLayers(layer1, layer2){
+  for(nodeIndex = 0; nodeIndex < layer2.length; nodeIndex++){
+    let node = layer2[nodeIndex];
+    let sumOfWeights = node.weights.reduce((acc, weight, index) => {
+      return acc + (weight * layer1[index].value);
+    }, 0);
+    
+    node.netValue = sumOfWeights + node.bias;
+    node.value = sigmoid(node.netValue);
+  };
+}
+
+// Based vaguely on https://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/
+function calculateError(model, exPair){
+  const inputCount = model[0].length;
+  const outputCount = model[model.length - 1].length;
+
+  let expectedOutput = exPair.slice(inputCount, inputCount + outputCount);
+  let layerIndex = model.length - 1;
+
+  let totalError = 0;
+  let outputLayer = model[layerIndex];
+  for (let nodeIndex = 0; nodeIndex < outputLayer.length; nodeIndex++){
+
+    outputLayer[nodeIndex].error = expectedOutput[nodeIndex] - outputLayer[nodeIndex].value;
+
+    // We need to total error of the layer for later
+    totalError += Math.pow(outputLayer[nodeIndex].error, 2);
+  }
+
+  return totalError;
+}
+
+function backwardPass(model, exPair, eta){
+  let totalError = calculateError(model, exPair);
+
+  // ----- Output Layer -----
+  const outputLayerIndex = model.length - 1;
+  for (let outputNodeIndex = 0; outputNodeIndex < model[outputLayerIndex].length; outputNodeIndex++){
+    let node = model[outputLayerIndex][outputNodeIndex];
+    let partialDerivativeOfNode = slope(sigmoid, node.netValue) * node.error;
+    node.derivative = partialDerivativeOfNode;
+    
+    for (let weightIndex = 0; weightIndex < node.weights.length; weightIndex++){
+      let weightDelta = eta * model[outputLayerIndex - 1][weightIndex].value * partialDerivativeOfNode;
+      node.weights[weightIndex] += weightDelta;
+    }
+
+    // For the purposes of back propagation the bias can be thought of as a weight with a 
+    // value that is always 1, hence the below
+    let biasDelta = eta * 1 * partialDerivativeOfNode;
+    node.bias += biasDelta;
+  }
+
+  // ----- Hidden Layers ----- //
+  // layerIndex > 0 ignores the first layers since we don't care about the input layer
+  for (let layerIndex = outputLayerIndex - 1; layerIndex > 0; layerIndex--){
+    for (let nodeIndex = 0; nodeIndex < model[layerIndex].length; nodeIndex++){
+      let node = model[layerIndex][nodeIndex];
+
+      let sumOfForwardLayersNodesPartialDerivativesAndWeights = model[layerIndex + 1].reduce((acc, forwardNode) => {
+        //console.log(forwardNode.derivative, forwardNode.weights[nodeIndex]);
+        return acc + (forwardNode.derivative * forwardNode.weights[nodeIndex]);
+      },0);
+
+      let partialDerivativeOfNode = slope(sigmoid, node.netValue) * sumOfForwardLayersNodesPartialDerivativesAndWeights;
+      node.derivative = partialDerivativeOfNode;
+
+      for (let weightIndex = 0; weightIndex < node.weights.length; weightIndex++){
+        //console.log(layerIndex, nodeIndex, weightIndex)
+
+        let weightDelta = eta * model[layerIndex - 1][weightIndex].value * partialDerivativeOfNode
+        node.weights[weightIndex] += weightDelta;
+      }
+
+      // For the purposes of back propagation the bias can be thought of as a weight with a 
+      // value that is always 1, hence the below
+      let biasDelta = eta * partialDerivativeOfNode;
+      node.bias += biasDelta;
+    }
+  }
+
+  return totalError;
+}
+
+function sigmoid(x) {
+  return 1 / ( 1 + Math.pow( Math.E, -1 * x) );
+}
+
+function slope (f, x, dx) {
+  dx = dx || 0.00000000000001;
+  return (f(x+dx) - f(x)) / dx;
+}
+
+/*
+// From: https://math.stackexchange.com/questions/78575/derivative-of-sigmoid-function-sigma-x-frac11e-x
+//0.21492890298354272
+function slope (f, x) {
+  return f(x) * (1 - f(x));
+}
+*/
 
 function evaluateOnList(model, exPairList, presentations){
   let result = {
@@ -92,115 +217,6 @@ function evaluateOnList(model, exPairList, presentations){
 
   return result;
 }
-
-function forwardPass(model, inputData){
-  inputData.forEach((dataPoint, index) => {
-    if (index < model[0].length){
-      model[0][index].value = dataPoint;
-    }
-  })
-
-  for (let i = 0; i < model.length - 1; i++){
-    calculateLayers(model[i], model[i+1])
-  }
-
-  //Return an array of the values of the nodes
-  return model[model.length -1].map(node => {
-    return node.value;
-  })
-}
-
-function calculateLayers(layer1, layer2){
-  for(nodeIndex = 0; nodeIndex < layer2.length; nodeIndex++){
-    let node = layer2[nodeIndex];
-    let sumOfWeights = node.weights.reduce((acc, weight, index) => {
-      return acc + (weight * layer1[index].value);
-    }, 0);
-    
-    node.netValue = sumOfWeights + node.bias;
-    node.value = sigmoid(sumOfWeights + node.bias);
-  };
-}
-
-function backwardPass(model, exPair, eta){
-  let totalError = calculateError(model, exPair);
-
-  // ----- Output Layer -----
-  const outputLayerIndex = model.length - 1;
-  for (let outputNodeIndex = 0; outputNodeIndex < model[outputLayerIndex].length; outputNodeIndex++){
-    let node = model[outputLayerIndex][outputNodeIndex];
-    let partialDerivativeOfNode = slope(sigmoid, node.netValue) * node.error;
-    node.derivative = partialDerivativeOfNode;
-    
-    for (let weightIndex = 0; weightIndex < node.weights.length; weightIndex++){
-      let weightDelta = eta * model[outputLayerIndex - 1][weightIndex].value * partialDerivativeOfNode;
-      node.weights[weightIndex] += weightDelta;
-    }
-  }
-
-  // ----- Hidden Layers ----- //
-  // layerIndex > 0 ignores the first layers since we don't care about the input layer
-  for (let layerIndex = outputLayerIndex - 1; layerIndex > 0; layerIndex--){
-    for (let nodeIndex = 0; nodeIndex < model[layerIndex].length; nodeIndex++){
-      let node = model[layerIndex][nodeIndex];
-
-      for (let weightIndex = 0; weightIndex < node.weights.length; weightIndex++){
-        //console.log(layerIndex, nodeIndex, weightIndex)
-    
-        let sumOfForwardLayersNodesPartialDerivativesAndWeights = model[layerIndex + 1].reduce((acc, forwardNode) => {
-          //console.log(forwardNode.derivative, forwardNode.weights[nodeIndex]);
-          return acc + (forwardNode.derivative * forwardNode.weights[nodeIndex]);
-        },0);
-
-        let partialDerivativeOfNode = slope(sigmoid, node.netValue) * sumOfForwardLayersNodesPartialDerivativesAndWeights;
-        node.derivative = partialDerivativeOfNode;
-
-        let weightDelta = eta * model[layerIndex - 1][weightIndex].value * partialDerivativeOfNode
-        node.weights[weightIndex] += weightDelta;
-      }
-    }
-  }
-
-  return totalError;
-}
-
-// Based vaguely on https://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/
-function calculateError(model, exPair){
-  const inputCount = model[0].length;
-  const outputCount = model[model.length - 1].length;
-
-  let expectedOutput = exPair.slice(inputCount, inputCount + outputCount);
-  let layerIndex = model.length - 1;
-
-  let totalError = 0;
-  let outputLayer = model[layerIndex];
-  for (let nodeIndex = 0; nodeIndex < outputLayer.length; nodeIndex++){
-
-    outputLayer[nodeIndex].error = expectedOutput[nodeIndex] - outputLayer[nodeIndex].value;
-
-    // We need to total error of the layer for later
-    totalError += Math.pow(outputLayer[nodeIndex].error, 2);
-  }
-
-  return totalError;
-}
-
-function sigmoid(x) {
-  return 1 / ( 1 + Math.pow( Math.E, -1 * x) );
-}
-
-function slope (f, x, dx) {
-  dx = dx || 0.00000000000001;
-  return (f(x+dx) - f(x)) / dx;
-}
-
-/*
-// From: https://math.stackexchange.com/questions/78575/derivative-of-sigmoid-function-sigma-x-frac11e-x
-//0.21492890298354272
-function slope (f, x) {
-  return f(x) * (1 - f(x));
-}
-*/
 
 function getOutputValues(model){
   return model[model.length - 1].reduce((acc, node) => {
